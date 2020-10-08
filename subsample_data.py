748eee0d409b85f367d225e4b2a84b4c8c0031ee
mkoretsky1 <54589102+mkoretsky1@users.noreply.github.com>
2020-10-08 12:40:40 -0400

    Subsample script

---
+++ subsample_data.py
@@ -0,0 +1,14 @@
+""" CS 254 - Final Project - Data Cleaning/Exploration """
+
+### Imports ###
+import pandas as pd
+
+df = pd.read_csv('nypd_data/NYPD_Complaint_Data_Historic.csv')
+
+df_10000 = df.sample(n=10000)
+df_100000 = df.sample(n=100000)
+df_1000000 = df.sample(n=1000000)
+
+df_10000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_10000_subsamples.csv')
+df_100000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_100000_subsamples.csv')
+df_1000000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_1000000_subsamples.csv')
