5e4cbda2abb60c5053d351ebc5c6aef0e8bdcb8b
lowelldeschenes <lmdesche@uvm.edu>
2020-10-13 12:21:04 -0400

    Update subsample_data.py

--- subsample_data.py
+++ subsample_data.py
@@ -3,14 +3,14 @@
 ### Imports ###
 import pandas as pd
 
-#df = pd.read_csv('nypd_data/NYPD_Complaint_Data_Historic.csv')
+df = pd.read_csv('nypd_data/NYPD_Complaint_Data_Historic.csv')
 
-#df_10000 = df.sample(n=10000)
-#df_100000 = df.sample(n=100000)
-#df_1000000 = df.sample(n=1000000)
+df_10000 = df.sample(n=10000)
+df_100000 = df.sample(n=100000)
+df_1000000 = df.sample(n=1000000)
 
-#df_10000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_10000_subsamples.csv')
-#df_100000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_100000_subsamples.csv')
-#df_1000000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_1000000_subsamples.csv')
+df_10000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_10000_subsamples.csv')
+df_100000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_100000_subsamples.csv')
+df_1000000.to_csv('nypd_data/NYPD_Complaint_Data_Historic_1000000_subsamples.csv')
 
-df = pd.read_csv('Users/lowell/desktop/Machine Learning/nypd_data/NYPD_Complaint_Data_Historic_10000_subsample.csv')
+df = pd.read_csv('nypd_data/NYPD_Complaint_Data_Historic_10000_subsamples.csv')
