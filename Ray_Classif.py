3e67aed442d51bbb7edb30798b767ffb929618e6
lowelldeschenes <lmdesche@uvm.edu>
2020-10-27 22:10:38 -0400

    getting confusion matrices for remaining models

--- Ray_Classif.py
+++ Ray_Classif.py
@@ -3,6 +3,7 @@
 ### Imports ###
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import scipy.stats as sstats
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
@@ -10,6 +11,8 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import classification_report
 
 
 ### Functions ###
@@ -23,17 +26,28 @@ def standardize(X_train, X_test):
     return X_train, X_test
 
 ### Main ###
-nypd = pd.read_csv('nypd_data/nypd_10000', parse_dates=['complaint_datetime'])
+nypd = pd.read_csv('nypd_data/nypd_10000.csv', parse_dates=['complaint_datetime'])
 nypd = nypd.dropna()
 print(len(nypd))
 
 # Getting X data
-drop = ['CMPLNT_NUM','complaint_datetime','boro_BRONX','boro_BROOKLYN','boro_MANHATTAN',
-        'boro_QUEENS','boro_STATEN ISLAND','Latitude','Longitude']
+# Variables to drop regardless of the analysis
+drop_always = ['CMPLNT_NUM','complaint_datetime','BORO_NM','time_of_day','season','tod_afternoon',
+               'tod_morning','tod_night','season_fall','season_spring','season_summer','season_winter']
+# Variables to drop when performing classification for location
+drop_for_location_analysis = ['Latitude','Longitude','boro_BRONX','boro_BROOKLYN','boro_MANHATTAN',
+                              'boro_QUEENS','boro_STATEN ISLAND']
+# Variables to drop when performing classification for tod
+drop_for_tod_analysis = ['hour','minute']
+# Variables to drop when performing classification for season
+drop_for_season_analysis = ['month']
+# Creating one list of variables to drop - Edit this line based on analysis being performed
+drop = drop_always + drop_for_location_analysis
 X = nypd.drop(drop, axis=1)
 
+
 # Response variable - starting with Manhattan
-y = nypd['boro_BRONX'].values
+y = nypd['BORO_NM'].values
 
 # Splitting
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=10)
@@ -49,6 +63,14 @@ rf_cv.fit(X_train, y_train)
 pred = rf_cv.predict(X_test)
 print(accuracy_score(y_test, pred))
 
+#lets get the full report
+print(classification_report(y_test, pred))
+
+#create visualization for confusion matrix
+fig, ax = plt.subplots(figsize=(10, 8))
+plot_confusion_matrix(rf_cv,X_test,y_test, ax = ax)
+plt.title('Random Forest Confusion Matrix')
+plt.show()
 
 # Trying an SVM classifier
 sv = SVC(kernel = 'linear')
@@ -56,8 +78,26 @@ sv.fit(X_train, y_train)
 svm_p = sv.predict(X_test)
 print(accuracy_score(y_test,svm_p))
 
+#lets get the full report
+print(classification_report(y_test, svm_p))
+
+#create visualization for confusion matrix
+fig, ax = plt.subplots(figsize=(10, 8))
+plot_confusion_matrix(sv,X_test,y_test, ax = ax)
+plt.title('SVM Confusion Matrix')
+plt.show()
+
 # Logistic Regression
-log_r = LogisticRegression()
+log_r = LogisticRegression(max_iter=100000)
 log_r.fit(X_train,y_train)
 classifier = log_r.predict(X_test)
-print(accuracy_score(y_test,log_r))
+print(accuracy_score(y_test,classifier))
+
+#lets get the full report
+print(classification_report(y_test, classifier))
+
+#create visualization for confusion matrix
+fig, ax = plt.subplots(figsize=(10, 8))
+plot_confusion_matrix(log_r,X_test,y_test, ax = ax)
+plt.title('Logistic Regression Confusion Matrix')
+plt.show()
